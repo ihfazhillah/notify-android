7f368875a4177322ff44f3e98480888b7ce82771
ihfazhillah <mihfazhillah@gmail.com>
2023-01-02 18:09:30 +0700

    Try to fix parse error

--- app/src/main/java/com/ihfazh/notify/feed/SimpleFeedItem.kt
+++ app/src/main/java/com/ihfazh/notify/feed/SimpleFeedItem.kt
@@ -1,6 +1,7 @@
 package com.ihfazh.notify.feed
 
 import android.annotation.SuppressLint
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -15,7 +16,12 @@ data class SimpleFeedItem(
       /*
       sample 2022-12-30T11:31:09.000004+07:00
        */
-      val fmt = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXX")
-      return fmt.parse(published)
+      return try{
+         val fmt = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXX")
+         fmt.parse(published)
+      } catch (e: ParseException){
+         val fmt = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")
+         fmt.parse(published)
+      }
    }
 }
